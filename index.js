ed26876a2b49f55d2736dcb99b8020b1b841e551
Niall Phelan <niall@medtec.ie>
2020-03-08 15:57:34 +0000

    allowance added for localhost port 3000 or env variable

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const credentials = {
 };
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+    port: process.env.PORT || 3000
+   // host: 'localhost'
 });
 
 require('./app/models/db');  //required to run mongodb
