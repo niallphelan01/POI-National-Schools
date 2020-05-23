6310b510d36924edb0f15b125d361d303d2bae6e
Niall Phelan <niall@medtec.ie>
2020-05-23 12:24:50 +0100

    Added tests for the new seeded data

--- routes-api.js
+++ routes-api.js
@@ -2,6 +2,6 @@ const Users = require('./app/api/users');
 
 module.exports = [
   { method: 'GET', path: '/api/users', config: Users.find }, //lists out all users
-  { method: 'GET', path: '/api/users/{id}', config: Users.findOne } //list out just one user defined by the id of the user
+  { method: 'GET', path: '/api/users/{id}', config: Users.findOne }, //list out just one user defined by the id of the user
 
 ];
--- test/userapitest.js
+++ test/userapitest.js
@@ -9,25 +9,36 @@ suite('User API tests', function () {
   test('get users', async function () {
     const response = await axios.get('http://localhost:3000/api/users');
     const users = response.data;
-    assert.equal(6, users.length);
+    assert.equal(3, users.length);
 
-    assert.equal(users[0].firstName, 'test');
+    assert.equal(users[0].firstName, 'basic');
     assert.equal(users[0].lastName, 'test');
-    assert.equal(users[0].email, 'test21@test.com');
+    assert.equal(users[0].email, 'basic@test.com');
     assert.equal(users[0].level, 'basic');
+
+    assert.equal(users[1].firstName, 'admin');
+    assert.equal(users[1].lastName, 'test');
+    assert.equal(users[1].email, 'admin@test.com');
+    assert.equal(users[1].level, 'admin');
+
+    assert.equal(users[2].firstName, 'superadmin');
+    assert.equal(users[2].lastName, 'test');
+    assert.equal(users[2].email, 'superadmin@test.com');
+    assert.equal(users[2].level, 'superAdmin');
   });
   test('get one user', async function () {
     let response = await axios.get('http://localhost:3000/api/users');
     const users = response.data;
-    assert.equal(6, users.length);
+    assert.equal(3, users.length);
 
     const oneUserUrl = 'http://localhost:3000/api/users/' + users[0]._id;
     response = await axios.get(oneUserUrl);
     const oneUser = response.data;
 
-    assert.equal(oneUser.firstName, 'test');
+    assert.equal(oneUser.firstName, 'basic');
     assert.equal(oneUser.lastName, 'test');
-    assert.equal(oneUser.email, 'test21@test.com');
+    assert.equal(oneUser.email, 'basic@test.com');
+
   });
 
 });
